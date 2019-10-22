9b3e522db3906211761b68949268019fdd75911d
HazeMSR <hazelmsr@gmail.com>
2019-10-22 08:53:44 -0500

    servidor

--- sincronizacion/src/main/java/Cliente.java
+++ sincronizacion/src/main/java/Cliente.java
@@ -10,38 +10,14 @@ public class Cliente{
  
         try (Socket socket = new Socket(DIRECCION, PUERTO)) {
             
-            //Obtiene el flujo de entrada del socket
-            InputStream flujo_entrada = socket.getInputStream();
-            
-            //Manda la peticion al socket y la recibe en un buffer de lectura
-            BufferedReader lector = new BufferedReader(new InputStreamReader(flujo_entrada));
-            
-            //Transforma el buffer de lectura a una cadena
-            String tiempo = lector.readLine();
-
-            //Transforma el tiempo que mando el servidor como cadena a instante
-            Instant tiempo_servidor = Instant.parse(tiempo);
-
-            //Obtiene el tiempo actual despues de terminar la peticion de hacer la peticion
-            Instant tiempo_despues = Instant.now();
-            
-            //Obtiene el contador de los milisegundos de la hora del servidor
-            long miliseg_serv = tiempo_servidor.toEpochMilli();
-            //Obtiene el contador de los milisegundos de la hora obtenida despues de llamar al servidor
-            long miliseg_desp = tiempo_despues.toEpochMilli();
-            //Resta los contadores para obtener la diferencia de tiempo
-            long diferencia_tiempo = miliseg_despues - miliseg_serv;
-            
-            System.out.println("Tiempo del Servidor UTC:", tiempo_servidor);
-            System.out.println("Tiempo actual: ", tiempo_despues);
-            System.out.println("Diferencia de milisegundos: ", diferencia_tiempo);
-            
-            //Nota: Originalmente este metodo no debia de ser main,
-            //si no una funcion llamada pedir tiempo que regrese la variable diferencia_tiempo
-            //esta variable del tipo long se le puede sumar a cualquier instante con el metodo
-            //instante_de_tiempo.plusMillis(diferencia_tiempo);
-            //donde instante_de_tiempo es la variable del tipo Instant que le queremos sumar el error
-            //al recibir la hora del servidor
+                OutputStream output = socket.getOutputStream();
+                PrintWriter writer = new PrintWriter(output, true);
+                //Primero se le manda la cantidad de clientes para sacar el promedio
+                writer.println("3");
+                //Despues se le mandan las horas de los clientes
+                writer.println(Instant.now().toString());
+                writer.println(Instant.now().toString());
+                writer.println(Instant.now().toString());
  
         } catch (UnknownHostException ex) {
  
--- sincronizacion/src/main/java/ServidorUtc.java
+++ sincronizacion/src/main/java/ServidorUtc.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.net.*;
 import java.time.Instant;
+import java.util.ArrayList;
 
 public class ServidorUtc {
     public static final int PUERTO = 9090;
@@ -9,15 +10,52 @@ public class ServidorUtc {
         try (ServerSocket serverSocket = new ServerSocket(PUERTO)) {
  
             System.out.println("El servidor esta corriendo en el puerto " + PUERTO);
- 
+            int num_clientes = 0;
+            int i =0;
+            ArrayList<Long> tiempos_clientes = new ArrayList<Long>();
             while (true) {
                 Socket socket = serverSocket.accept();
                 System.out.println("Nuevo cliente conectado!");
- 
-                OutputStream output = socket.getOutputStream();
-                PrintWriter writer = new PrintWriter(output, true);
- 
-                writer.println(Instant.now().toString());
+                               
+                //Obtiene el flujo de entrada del socket
+            InputStream flujo_entrada = socket.getInputStream();
+            
+            //Manda la peticion al socket y la recibe en un buffer de lectura
+            BufferedReader lector = new BufferedReader(new InputStreamReader(flujo_entrada));
+            
+            //Transforma el buffer de lectura a una cadena
+            String linea = lector.readLine();
+            
+            if(linea.matches("[0-9]+")){
+                num_clientes = Integer.valueOf(linea);
+                tiempos_clientes = new ArrayList<Long>();
+            }else{
+            //Transforma el tiempo que mando el servidor como cadena a instante
+            Instant tiempo_servidor = Instant.parse(linea);
+
+            //Obtiene el tiempo actual despues de terminar la peticion de hacer la peticion
+            Instant tiempo_despues = Instant.now();
+            
+            //Obtiene el contador de los milisegundos de la hora del servidor
+            long miliseg_serv = tiempo_servidor.toEpochMilli();
+            //Obtiene el contador de los milisegundos de la hora obtenida despues de llamar al servidor
+            long miliseg_desp = tiempo_despues.toEpochMilli();
+            //Resta los contadores para obtener la diferencia de tiempo
+            long diferencia_tiempo = miliseg_desp - miliseg_serv;
+            
+            System.out.println("Tiempo del Servidor UTC:"+ tiempo_servidor);
+            System.out.println("Tiempo actual: "+ tiempo_despues);
+            System.out.println("Diferencia de milisegundos: "+ diferencia_tiempo);
+            tiempos_clientes.add(diferencia_tiempo);
+            i++;
+            if(i == num_clientes){
+                long promedio = 0;
+                for(int j=0; j < tiempos_clientes.size();j++){
+                    promedio += tiempos_clientes.get(j);
+                }
+                promedio /= tiempos_clientes.size();
+            }
+            }
             }
  
         } catch (IOException ex) {
