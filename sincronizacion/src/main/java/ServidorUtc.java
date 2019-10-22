1c73e42eca54f4c8650d61c70090bd9cf920547d
HazeMSR <hazelmsr@gmail.com>
2019-10-22 13:24:36 -0500

    Identacion

--- sincronizacion/src/main/java/ServidorUtc.java
+++ sincronizacion/src/main/java/ServidorUtc.java
@@ -18,44 +18,44 @@ public class ServidorUtc {
                 System.out.println("Nuevo cliente conectado!");
                                
                 //Obtiene el flujo de entrada del socket
-            InputStream flujo_entrada = socket.getInputStream();
-            
-            //Manda la peticion al socket y la recibe en un buffer de lectura
-            BufferedReader lector = new BufferedReader(new InputStreamReader(flujo_entrada));
-            
-            //Transforma el buffer de lectura a una cadena
-            String linea = lector.readLine();
-            
-            if(linea.matches("[0-9]+")){
-                num_clientes = Integer.valueOf(linea);
-                tiempos_clientes = new ArrayList<Long>();
-            }else{
-            //Transforma el tiempo que mando el servidor como cadena a instante
-            Instant tiempo_servidor = Instant.parse(linea);
+                InputStream flujo_entrada = socket.getInputStream();
+                
+                //Manda la peticion al socket y la recibe en un buffer de lectura
+                BufferedReader lector = new BufferedReader(new InputStreamReader(flujo_entrada));
+                
+                //Transforma el buffer de lectura a una cadena
+                String linea = lector.readLine();
+                
+                if(linea.matches("[0-9]+")){
+                    num_clientes = Integer.valueOf(linea);
+                    tiempos_clientes = new ArrayList<Long>();
+                }else{
+                    //Transforma el tiempo que mando el servidor como cadena a instante
+                    Instant tiempo_servidor = Instant.parse(linea);
 
-            //Obtiene el tiempo actual despues de terminar la peticion de hacer la peticion
-            Instant tiempo_despues = Instant.now();
-            
-            //Obtiene el contador de los milisegundos de la hora del servidor
-            long miliseg_serv = tiempo_servidor.toEpochMilli();
-            //Obtiene el contador de los milisegundos de la hora obtenida despues de llamar al servidor
-            long miliseg_desp = tiempo_despues.toEpochMilli();
-            //Resta los contadores para obtener la diferencia de tiempo
-            long diferencia_tiempo = miliseg_desp - miliseg_serv;
-            
-            System.out.println("Tiempo del Servidor UTC:"+ tiempo_servidor);
-            System.out.println("Tiempo actual: "+ tiempo_despues);
-            System.out.println("Diferencia de milisegundos: "+ diferencia_tiempo);
-            tiempos_clientes.add(diferencia_tiempo);
-            i++;
-            if(i == num_clientes){
-                long promedio = 0;
-                for(int j=0; j < tiempos_clientes.size();j++){
-                    promedio += tiempos_clientes.get(j);
+                    //Obtiene el tiempo actual despues de terminar la peticion de hacer la peticion
+                    Instant tiempo_despues = Instant.now();
+                    
+                    //Obtiene el contador de los milisegundos de la hora del servidor
+                    long miliseg_serv = tiempo_servidor.toEpochMilli();
+                    //Obtiene el contador de los milisegundos de la hora obtenida despues de llamar al servidor
+                    long miliseg_desp = tiempo_despues.toEpochMilli();
+                    //Resta los contadores para obtener la diferencia de tiempo
+                    long diferencia_tiempo = miliseg_desp - miliseg_serv;
+                    
+                    System.out.println("Tiempo del Servidor UTC:"+ tiempo_servidor);
+                    System.out.println("Tiempo actual: "+ tiempo_despues);
+                    System.out.println("Diferencia de milisegundos: "+ diferencia_tiempo);
+                    tiempos_clientes.add(diferencia_tiempo);
+                    i++;
+                    if(i == num_clientes){
+                        long promedio = 0;
+                        for(int j=0; j < tiempos_clientes.size();j++){
+                            promedio += tiempos_clientes.get(j);
+                        }
+                        promedio /= tiempos_clientes.size();
+                    }
                 }
-                promedio /= tiempos_clientes.size();
-            }
-            }
             }
  
         } catch (IOException ex) {
